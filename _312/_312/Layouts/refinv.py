ce81919e48a25b8af76c6d08ecee9ab30ccc7545
Aldo Quintal <aldo.quintal@simdatagroup.com>
2024-06-18 11:50:23 -0600

    refinv

--- _312/_312/Layouts/refinv.py
+++ _312/_312/Layouts/refinv.py
@@ -1,108 +1,93 @@
 from pyspark.sql import functions as F
 from pyspark.sql.types import DecimalType, DateType, StringType, DoubleType
 from Funciones import FuncionesExternas as FE, Sentencias as s, conexion as c
+import logging
+
+# Configure logger
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
 
 def refinv01(spark, datasource, columnas, client, branch, report):
-    # Carga funciones definidas
+    # Load defined functions
     LCodigos = F.udf(lambda z: FE.LimpiaCodigos(z), StringType())
     LTexto = F.udf(lambda z: FE.LimpiaTexto(z), StringType())
     LEmail = F.udf(lambda z: FE.LimpiaEmail(z), StringType())
 
-    # Lee el archivo import.csv para obtener los nombres de las columnas
+    # Read the import.csv file to get column names
     imp = spark.read.option("header", True).csv(columnas)
     nombresColumnas = [row[1] for row in imp.collect()]
 
-    # Lee y procesa los datos desde el archivo fuente
+    # Read and process data from the source file
     data = spark.read.text(datasource)
     data = data.withColumn("columns", F.split(data["value"], "\\|"))
     expresiones = [f"columns[{i}] as {columna}" for i, columna in enumerate(nombresColumnas)]
     data = data.selectExpr(*expresiones)
 
-    # Realiza las transformaciones en cadena
+    # Apply transformations
     data = data.withColumn("Client", F.lit(client))
     data = data.withColumn("Branch", F.lit(branch))
-    data = data.withColumn("Date", F.lit(F.current_date()))
-    
-    data = data.withColumn("NumeroParte", LTexto(F.col("NumeroParte")))
-    data = data.withColumn("Descripcion", F.substring( F.col("Descripcion").cast("string"),1,30))
-    data = data.withColumn("Descripcion", LTexto(F.col("Descripcion")))
-    data = data.withColumn("TipoParte", LTexto(F.col("TipoParte")))
-    data = data.withColumn("Almacen", F.substring( F.col("Almacen").cast("string"),1,10))
-    data = data.withColumn("Almacen", LTexto(F.col("Almacen")))
-    data = data.withColumn("Existencia", F.col("Existencia").cast(DecimalType(15,4)))
-    data = data.withColumn("`CostoUnit$`",  F.col("`CostoUnit$`").cast(DecimalType(18,4)))
-    data = data.withColumn("`Costo$`", F.col("`Costo$`").cast(DecimalType(35,10)))
-    data = data.withColumn("`Precio$`", F.col("`Precio$`").cast(DecimalType(18,4)))
-    data = data.withColumn("`Precio2$`", F.col("`Precio2$`").cast(DecimalType(18,4)))
-    data = data.withColumn("`Precio3$`", F.col("`Precio3$`").cast(DecimalType(18,4)))
-    data = data.withColumn("`Precio4$`",LTexto(F.col("`Precio4$`")))
-    data = data.withColumn(
-                        "`Precio4$`",
-                        F.when(F.isnull(F.col("`Precio4$`")) | (F.col("`Precio4$`") == ""), F.lit(0).cast(DoubleType()))
-                        .otherwise(F.col("`Precio4$`").cast(DoubleType()))
-                                )
-    #data = data.withColumn("`Precio4$`", F.when(F.col("`Precio4$`").isNull() | (F.col("`Precio4$`") == ''), 0).otherwise(F.col("`Precio4$`")))
-    #data = data.withColumn("`Precio4$`", F.col("`Precio4$`").cast(DecimalType(18,4)))
-    #data = data.withColumn("`Precio4$`", F.when(F.col("`Precio4$`").isNotNull(), F.col("`Precio4$`")).otherwise(0).cast(DecimalType(18,4)))
-    #sdata = data.withColumn("`Precio5$`", F.col("`Precio5$`").cast(DecimalType(18,4)))
-    data = data.withColumn("UltimaCompra", F.to_date(F.col("UltimaCompra"), "dd/MM/yyyy").cast(DateType()))
-    data = data.withColumn("UltimaVenta", F.to_date(F.col("UltimaVenta"), "dd/MM/yyyy").cast(DateType()))
-    data = data.withColumn("FechaAlta", F.to_date(F.col("FechaAlta"), "dd/MM/yyyy").cast(DateType()))
-    #data = data.withColumn("Clasificacion", LTexto(F.col("Clasificacion")))
+    data = data.withColumn("Date", F.current_date())
 
-    #data.show()
-    # Lee las columnas desde el archivo exportar.csv
+    data = (data
+            .withColumn("NumeroParte", LTexto(F.col("NumeroParte")))
+            .withColumn("Descripcion", LTexto(F.substring(F.col("Descripcion").cast("string"), 1, 30)))
+            .withColumn("TipoParte", LTexto(F.col("TipoParte")))
+            .withColumn("Almacen", LTexto(F.substring(F.col("Almacen").cast("string"), 1, 10)))
+            .withColumn("Existencia", F.col("Existencia").cast(DecimalType(15, 4)))
+            .withColumn("`CostoUnit$`", F.col("`CostoUnit$`").cast(DecimalType(18, 4)))
+            .withColumn("`Costo$`", F.col("`Costo$`").cast(DecimalType(35, 10)))
+            .withColumn("`Precio$`", F.col("`Precio$`").cast(DecimalType(18, 4)))
+            .withColumn("`Precio2$`", F.col("`Precio2$`").cast(DecimalType(18, 4)))
+            .withColumn("`Precio3$`", F.col("`Precio3$`").cast(DecimalType(18, 4)))
+            .withColumn("`Precio4$`", F.when(F.isnull(F.col("`Precio4$`")) | (F.col("`Precio4$`") == ""), F.lit(0).cast(DoubleType()))
+                        .otherwise(F.col("`Precio4$`").cast(DoubleType())))
+            .withColumn("UltimaCompra", F.to_date(F.col("UltimaCompra"), "dd/MM/yyyy").cast(DateType()))
+            .withColumn("UltimaVenta", F.to_date(F.col("UltimaVenta"), "dd/MM/yyyy").cast(DateType()))
+            .withColumn("FechaAlta", F.to_date(F.col("FechaAlta"), "dd/MM/yyyy").cast(DateType()))
+           )
+
+    # Read export.csv to get export column names
     expor = spark.read.option("header", True).csv(columnas)
     nombresColumnasExp = [row[3] for row in expor.collect()]
 
-    # Selecciona las columnas necesarias y exporta los datos
+    # Select and export necessary columns
     export = data.select([F.col(columna) for columna in nombresColumnasExp])
-    nombresColumnasExp = [row[3] for row in expor.collect()]
+    export = export.filter((F.col("NumeroParte") != "NUMERO") & (F.col("NumeroParte") != ""))
 
+    # Convert to Pandas DataFrame
+    logger.info("********** Transforming to Pandas DataFrame **********")
+    pandas_df = export.toPandas()
+    logger.info("Conversion Complete")
+    tuplas = list(pandas_df.itertuples(index=False, name=None))
+
+    # Database connection
+    c.cbase(client)
+    cnn = c.conect(client)
+
+    # Drop existing data and create new database structure
+    s.drop(report, branch, client, cnn)
+    s.create(columnas, report, branch, cnn)
+
+    # Insert data into database
+    logger.info(f"********** Adding records to {report} for {client} **********")
+    cursor = cnn.cursor()
     Exp = expor.groupBy().agg(F.concat_ws(",", F.collect_list("Col")).alias("cadena"))
     Exp2 = expor.groupBy().agg(F.concat_ws(",", F.collect_list("Col2")).alias("cadena2"))
-    
-    export = data.select([F.col(columna) for columna in nombresColumnasExp])
-    export=export.filter((F.col("NumeroParte") != "NUMERO") & (F.col("NumeroParte") != ""))
-    #export.show()
-    ########################################## Se pasa el archivo a DF y se obtienen los meses actualizados ###################
-    print("********** Transformado a Pandas DF **********")
-    pandas_df = export.toPandas()
-    print("ConversionFinalizada")
-    tuplas = list(pandas_df.itertuples(index=False,name=None))
-    
-
-
-    #################################### SE LLAMA LA CONEXION CON LA BASE DE DATOS #################################
-    c.cbase(client)
-
-    cnn=c.conect(client)
-
-    ################################### COMIENZA PROCESO DE BORRADO DE INFORMACION ##################################
-    s.drop(report,branch,client,cnn)
-
-    ################################### COMIENZA PROCESO DE CREACION DE DB ##################################
-    s.create(columnas,report,branch,cnn)
-
-    ############################## INSERCIÓN DA DATOS ######################################
-    print("********** Agregando registros..." + report + "" +branch+ " a la base: sim_" + client + " **********")
-    cursor = cnn.cursor()
 
     values = Exp.first()["cadena"]
     values2 = Exp2.first()["cadena2"]
-    #(Client,Branch,Date,NumeroOT,FechaEntrega,FechaCierre,Taller,TipoOrden,TipoServicio,Motivo,Status,NumeroParte,Descripcion,Cantidad,VentaUnit,Venta,CostoUnit,Costo,Utilidad,Margen,Vin,RFC,Modelo,Version)
     sql = f"INSERT INTO {report}{branch} ({values}) VALUES({values2})"
 
     try:
-        cursor.executemany(sql,tuplas)
+        cursor.executemany(sql, tuplas)
         cnn.commit()
-        print("********** Registros agregados correctamente **********")
-        
-    except:
+        logger.info("********** Records added successfully **********")
+    except Exception as e:
         cnn.rollback()
-        print("********** Error al cargar los registros **********")
-                #cnn.close()
-    ############################## INSERCIÓN DA DATOS ######################################
-    s.change(report, branch, cnn,columnas)
-    s.export(report,branch,cnn,client)
-    print("******************************* Finaliza Procesamiento " +report+ "" +branch+ " Del Cliente: " +client+ " ***********************************" )
+        logger.error(f"********** Error adding records: {e} **********")
+
+    # Apply changes and export data
+    s.change(report, branch, cnn, columnas)
+    s.export(report, branch, cnn, client)
+
+    logger.info(f"******************************* Processing complete for {report} {branch} for client: {client} ***********************************")
