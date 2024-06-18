44a1684440f93aa9c9936a16e89f2a7bc0bbeb8d
Aldo Quintal <aldo.quintal@simdatagroup.com>
2024-06-18 12:49:53 -0600

    vtausa

--- _312/_312/Layouts/vtausa.py
+++ _312/_312/Layouts/vtausa.py
@@ -9,137 +9,105 @@ def vtausa01(spark, datasource, columnas, client, branch, report):
 
     os.environ['PYSPARK_PYTHON'] = sys.executable
     os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
-    # Carga funciones definidas
-    LCodigos = F.udf(lambda z: FE.LimpiaCodigos(z), StringType())
-    LTexto = F.udf(lambda z: FE.LimpiaTexto(z), StringType())
-    LEmail = F.udf(lambda z: FE.LimpiaEmail(z), StringType())
 
-    # Lee el archivo import.csv para obtener los nombres de las columnas
+    # Load defined functions
+    LCodigos = F.udf(FE.LimpiaCodigos, StringType())
+    LTexto = F.udf(FE.LimpiaTexto, StringType())
+    LEmail = F.udf(FE.LimpiaEmail, StringType())
+
+    # Read import.csv to get column names
     imp = spark.read.option("header", True).csv(columnas)
-    print(f'Imp del reporte: {imp}')
     nombresColumnas = [row[1] for row in imp.collect()]
-    print(f'Nombre columnas: {nombresColumnas}')
 
-    # Lee y procesa los datos desde el archivo fuente
-    print(f'Datasource ... {datasource}')
-    print(f'Spark : {spark}')
-    print(f'Columnas: {columnas}')
-
+    # Read and process data from the source file
     data = spark.read.text(datasource)
-    print(f'Data read text: {data}')
     data = data.withColumn("columns", F.split(data["value"], "\\|"))
-    print(f'Data with columns: {data}')
     expresiones = [f"columns[{i}] as {columna}" for i, columna in enumerate(nombresColumnas)]
-    print(f'Expresiones: {expresiones}')
-
     data = data.selectExpr(*expresiones)
-    
-    data.show()
-    
-    print(F.col("Factura"))
-    # Realiza las transformaciones en cadena
-    data = data.withColumn("Client", F.lit(client))
-    print(f'data cliente: {data}')
-    data = data.withColumn("Branch", F.lit(branch))
-    print(f'data branch: {data} ')
-    data = data.withColumn("Date", F.lit(F.current_date()))
-    print(f'data date: {data}')
-    data = data.withColumn("Factura", LCodigos(F.col("Factura")))
-    print(f'data factura: {data}')
-    data = data.withColumn("FechaFactura", F.to_date(F.col("FechaFactura"), "dd/MM/yyyy").cast(DateType()))
-    data = data.withColumn("TipoVenta", LTexto(F.col("TipoVenta")))
-    data = data.withColumn("TipoPago", LTexto(F.col("TipoPago")))
-    data = data.withColumn("Vin", F.substring( F.col("Vin").cast("string"),1,20))
-    data = data.withColumn("Vin", LTexto(F.col("Vin")))
-    data = data.withColumn("NumeroInventario", LCodigos(F.col("NumeroInventario")))
-    data = data.withColumn("`Isan$`", F.when(F.col("`Isan$`").isNotNull(), F.col("`Isan$`")).otherwise(0).cast(DecimalType(18,4)))
-    data = data.withColumn("`Costo$`", F.when(F.col("`Costo$`").isNotNull(), F.col("`Costo$`")).otherwise(0).cast(DecimalType(35,10)))
-    data = data.withColumn("`Venta$`", F.when(F.col("`Venta$`").isNotNull(), F.col("`Venta$`")).otherwise(0).cast(DecimalType(35,10)))
-    data = data.withColumn("`Utilidad$`", F.when(F.col("`Utilidad$`").isNotNull(), F.col("`Venta$`")).otherwise(0).cast(DecimalType(18,4)))
-    data = data.withColumn("Margen", F.col("Margen").cast(DecimalType()))
-    data = data.withColumn("Margen", 
-                           F.when((F.col("`Costo$`")==0) | (F.col("`Venta$`")==0), 0)
-                           .otherwise(F.when(F.col("`Venta$`")<0,((F.col("`Venta$`")/F.col("`Costo$`"))-1)*-100).otherwise(((F.col("`Venta$`")/F.col("`Costo$`"))-1)*100)))
-    data = data.withColumn("Ano", LTexto(F.col("Ano")))
-    data = data.withColumn("Marca", LTexto(F.col("Marca")))
-    data = data.withColumn("Modelo", F.substring( F.col("Modelo").cast("string"),1,30))
-    data = data.withColumn("Modelo", LTexto(F.col("Modelo")))
-    data = data.withColumn("Color", LTexto(F.col("Color")))
-    data = data.withColumn("Interior", LTexto(F.col("Interior")))
-    data = data.withColumn("NumeroVendedor", LTexto(F.col("NumeroVendedor")))
-    data = data.withColumn("NombreVendedor", F.substring(F.col("NombreVendedor").cast("string"),1,30))
-    data = data.withColumn("NombreVendedor", LTexto(F.col("NombreVendedor")))
-    data = data.withColumn("FechaCompra", F.to_date(F.col("FechaCompra"), "dd/MM/yyyy").cast(DateType()))
-    data = data.withColumn("FechaEntrega", F.to_date(F.col("FechaEntrega"), "dd/MM/yyyy").cast(DateType()))
-    data = data.withColumn("NombreCliente", F.substring(F.col("NombreCliente").cast("string"), 1, 30))
-    data = data.withColumn("NombreCliente", LTexto(F.col("NombreCliente")))
-    data = data.withColumn("RFC", F.substring(F.col("RFC").cast("string"),1,13))
-    data = data.withColumn("RFC", LCodigos(F.col("RFC")))
-    data = data.withColumn("Direccion", LTexto(F.col("Direccion")))
-    data = data.withColumn("Telefono", LCodigos(F.col("Telefono")))
-    data = data.withColumn("CP", LTexto(F.col("CP")))
-    data = data.withColumn("Email", LEmail(F.col("Email")))
-    data = data.withColumn("VentasNetas", 
-                      F.when((F.col("`Venta$`") == 0) | (F.col("`Costo$`") == 0), 0)
-                      .when((F.col("`Venta$`") < 0) | (F.col("`Costo$`") < 0), -1)
-                      .otherwise(1)
-                      )
 
+    # Apply transformations
+    data = (data
+            .withColumn("Client", F.lit(client))
+            .withColumn("Branch", F.lit(branch))
+            .withColumn("Date", F.current_date())
+            .withColumn("Factura", LCodigos(F.col("Factura")))
+            .withColumn("FechaFactura", F.to_date(F.col("FechaFactura"), "dd/MM/yyyy").cast(DateType()))
+            .withColumn("TipoVenta", LTexto(F.col("TipoVenta")))
+            .withColumn("TipoPago", LTexto(F.col("TipoPago")))
+            .withColumn("Vin", F.substring(F.col("Vin").cast("string"), 1, 20))
+            .withColumn("Vin", LTexto(F.col("Vin")))
+            .withColumn("NumeroInventario", LCodigos(F.col("NumeroInventario")))
+            .withColumn("`Isan$`", F.coalesce(F.col("`Isan$`"), F.lit(0)).cast(DecimalType(18, 4)))
+            .withColumn("`Costo$`", F.coalesce(F.col("`Costo$`"), F.lit(0)).cast(DecimalType(35, 10)))
+            .withColumn("`Venta$`", F.coalesce(F.col("`Venta$`"), F.lit(0)).cast(DecimalType(35, 10)))
+            .withColumn("`Utilidad$`", F.coalesce(F.col("`Venta$`"), F.lit(0)).cast(DecimalType(18, 4)))
+            .withColumn("Margen", 
+                        F.when((F.col("`Costo$`") == 0) | (F.col("`Venta$`") == 0), 0)
+                        .otherwise(F.when(F.col("`Venta$`") < 0, ((F.col("`Venta$`") / F.col("`Costo$`")) - 1) * -100)
+                        .otherwise(((F.col("`Venta$`") / F.col("`Costo$`")) - 1) * 100))
+                       )
+            .withColumn("Ano", LTexto(F.col("Ano")))
+            .withColumn("Marca", LTexto(F.col("Marca")))
+            .withColumn("Modelo", F.substring(F.col("Modelo").cast("string"), 1, 30))
+            .withColumn("Modelo", LTexto(F.col("Modelo")))
+            .withColumn("Color", LTexto(F.col("Color")))
+            .withColumn("Interior", LTexto(F.col("Interior")))
+            .withColumn("NumeroVendedor", LTexto(F.col("NumeroVendedor")))
+            .withColumn("NombreVendedor", F.substring(F.col("NombreVendedor").cast("string"), 1, 30))
+            .withColumn("NombreVendedor", LTexto(F.col("NombreVendedor")))
+            .withColumn("FechaCompra", F.to_date(F.col("FechaCompra"), "dd/MM/yyyy").cast(DateType()))
+            .withColumn("FechaEntrega", F.to_date(F.col("FechaEntrega"), "dd/MM/yyyy").cast(DateType()))
+            .withColumn("NombreCliente", F.substring(F.col("NombreCliente").cast("string"), 1, 30))
+            .withColumn("NombreCliente", LTexto(F.col("NombreCliente")))
+            .withColumn("RFC", F.substring(F.col("RFC").cast("string"), 1, 13))
+            .withColumn("RFC", LCodigos(F.col("RFC")))
+            .withColumn("Direccion", LTexto(F.col("Direccion")))
+            .withColumn("Telefono", LCodigos(F.col("Telefono")))
+            .withColumn("CP", LTexto(F.col("CP")))
+            .withColumn("Email", LEmail(F.col("Email")))
+            .withColumn("VentasNetas", 
+                        F.when((F.col("`Venta$`") == 0) | (F.col("`Costo$`") == 0), 0)
+                        .when((F.col("`Venta$`") < 0) | (F.col("`Costo$`") < 0), -1)
+                        .otherwise(1)
+                       )
+           )
 
-    #data.show()
-    # Lee las columnas desde el archivo exportar.csv
+    # Read export.csv to get export column names
     expor = spark.read.option("header", True).csv(columnas)
     nombresColumnasExp = [row[3] for row in expor.collect()]
 
-    # Selecciona las columnas necesarias y exporta los datos
+    # Select necessary columns and filter data
     export = data.select([F.col(columna) for columna in nombresColumnasExp])
-    nombresColumnasExp = [row[3] for row in expor.collect()]
+    export = export.filter((F.col("Vin") != "VIN") & (F.col("Vin") != ""))
 
+    # Convert to Pandas DataFrame
+    pandas_df = export.toPandas()
+    tuplas = list(pandas_df.itertuples(index=False, name=None))
+
+    # Database connection
+    c.cbase(client)
+    cnn = c.conect(client)
+
+    # Drop existing data and create new database structure
+    s.drop(report, branch, client, cnn)
+    s.create(columnas, report, branch, cnn)
+
+    # Insert data into the database
+    cursor = cnn.cursor()
     Exp = expor.groupBy().agg(F.concat_ws(",", F.collect_list("Col")).alias("cadena"))
     Exp2 = expor.groupBy().agg(F.concat_ws(",", F.collect_list("Col2")).alias("cadena2"))
-    
-    export = data.select([F.col(columna) for columna in nombresColumnasExp])
-    export=export.filter((F.col("Vin") != "VIN") & (F.col("Vin") != ""))
-    #export.show()
-    ########################################## Se pasa el archivo a DF y se obtienen los meses actualizados ###################
-    print("********** Transformado a Pandas DF **********")
-    pandas_df = export.toPandas()
-    print("ConversionFinalizada")
-    tuplas = list(pandas_df.itertuples(index=False,name=None))
-    
-
-
-    #################################### SE LLAMA LA CONEXION CON LA BASE DE DATOS #################################
-    c.cbase(client)
-
-    print(f'Cliente Connect ......... {client}')
-    cnn=c.conect(client)
-
-    ################################### COMIENZA PROCESO DE BORRADO DE INFORMACION ##################################
-    s.drop(report,branch,client,cnn)
-
-    ################################### COMIENZA PROCESO DE CREACION DE DB ##################################
-    s.create(columnas,report,branch,cnn)
-
-    ############################## INSERCIÓN DA DATOS ######################################
-    print("********** Agregando registros..." + report + "" +branch+ " a la base: sim_" + client + " **********")
-    cursor = cnn.cursor()
 
     values = Exp.first()["cadena"]
     values2 = Exp2.first()["cadena2"]
-    #(Client,Branch,Date,NumeroOT,FechaEntrega,FechaCierre,Taller,TipoOrden,TipoServicio,Motivo,Status,NumeroParte,Descripcion,Cantidad,VentaUnit,Venta,CostoUnit,Costo,Utilidad,Margen,Vin,RFC,Modelo,Version)
     sql = f"INSERT INTO {report}{branch} ({values}) VALUES({values2})"
 
     try:
-        cursor.executemany(sql,tuplas)
+        cursor.executemany(sql, tuplas)
         cnn.commit()
         print("********** Registros agregados correctamente **********")
-    
     except:
         cnn.rollback()
         print("********** Error al cargar los registros **********")
-                #cnn.close()
-    ############################## INSERCIÓN DA DATOS ######################################
-    s.change(report, branch, cnn,columnas)
-    s.export(report,branch,cnn,client)
-    print("******************************* Finaliza Procesamiento " +report+ "" +branch+ " Del Cliente: " +client+ " ***********************************" )
+    s.change(report, branch, cnn, columnas)
+    s.export(report, branch, cnn, client)
+    print("******************************* Finaliza Procesamiento " + report + "" + branch + " Del Cliente: " + client + " ***********************************" )
