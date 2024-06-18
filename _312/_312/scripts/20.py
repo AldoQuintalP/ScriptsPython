fc68be86abd7451182b989011afbb95baacb5b5e
Aldo Quintal <aldo.quintal@simdatagroup.com>
2024-06-18 13:04:19 -0600

    refser

--- _312/_312/scripts/20.py
+++ _312/_312/scripts/20.py
@@ -73,7 +73,7 @@ else:
     ruta = r'/_312'
 
     #layouts=["invnue","invusa","refcom","refinv","refoep","seroep","sertec","vtanue","vtausa","refmos","refser"]
-    layouts=["refmos"]
+    layouts=["refser"]
 
 ######## For Interacción de Layouts#######
     for i in layouts:
